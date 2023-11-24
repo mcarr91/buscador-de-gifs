0a78e036eb48f4af9b6783ae114067ed9cc89c7e
Mauricio Carrizo <mauricio.carrizo@itscordoba.edu.ar>
2023-11-24 16:54:21 -0300

    se agregan los archivos al repo

---
+++ js/main.js
@@ -0,0 +1,28 @@
+const apikey = "EJzHESSo0QZ8BLk56AlufD1fqalLtJAC";
+const termino = document.querySelector(".termino");
+const buscador = document.querySelector(".buscador");
+const resultados = document.querySelector(".resultados");
+
+
+buscador.addEventListener("click", () => {
+    resultados.innerHTML = "";
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=${apikey}&q=${termino.value}&limit=9`;
+    const peticion = fetch(url);
+    peticion
+        .then((response) => {
+            return response.json();
+        })
+        .then((response) => {
+            response.data.forEach(element => {
+                const urlImg = element.images.original.url;
+                const img = document.createElement("img");
+                const div = document.createElement("div");
+                div.className = "gif-card";
+                img.src = urlImg;
+                resultados.appendChild(div);
+                div.appendChild(img);
+
+            });
+
+        })
+})
